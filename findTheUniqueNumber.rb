0f0dfbcd97031c84a3fc634a222a351d57cc1280
Eleazar Lorea <azarlorea1990@gmail.com>
2021-07-07 09:06:56 +0800

    first commit

--- findTheUniqueNumber.rb
+++ findTheUniqueNumber.rb
@@ -1,22 +1,20 @@
 def uniq(input)
-output=[]
-duplicate =[]
-(0..input.length).each do |number|
-if output.include?(input[number])
-    duplicate.push(input[number])
-    output.delete(input[number])
-else
-    output.push(input[number])
-end
-if duplicate.include?(output.last)
-    output.pop()
+    output=[]
+    duplicate =[]
+    (0..input.length).each do |number|
+    if output.include?(input[number])
+        duplicate.push(input[number])
+        output.delete(input[number])
+    else
+        output.push(input[number])
     end
-end
-
-return output
-end
-
-puts uniq([1,1,1,2,1,1])
-puts uniq([0,0,0.55,0,0])
-
-
+    if duplicate.include?(output.last)
+        output.pop()
+        end
+    end
+    
+    return output
+    end
+    
+    puts uniq([1,1,1,2,1,1])
+    puts uniq([0,0,0.55,0,0])
